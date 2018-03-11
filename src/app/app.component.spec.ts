129d9a5ccbf2c742bbf6af5ddf4c3c2041e1ae66
blancoparis-tfc <blancoparis.tfc@gmail.com>
2018-03-11 23:25:01 +0100

    Adaptado, los test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,15 +19,16 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
+  it('should render title in a mat-toolbar > mat-toolbar-row(1)', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('mat-toolbar > mat-toolbar-row:nth-child(1)').textContent).toContain('Titulo');
+  }));
+  it('should render title in a mat-toolbar > mat-toolbar-row(2)', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('mat-toolbar > mat-toolbar-row:nth-child(2)').textContent).toContain('D.B.P.');
   }));
 });
